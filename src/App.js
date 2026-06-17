897acb1fbc9904ec63e158b0d42da10d7a0a48a6
Ameya Kale <ameya.k.kale@gmail.com>
2026-06-16 23:43:20 -0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -6,38 +6,41 @@ import SearchResults from "./screens/SearchResults";
 import TvShows from "./screens/TvShows";
 import Layout from "./utils/Layout";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Trending />,
-      },
-      {
-        path: "/trending",
-        element: <Trending />,
-      },
-      {
-        path: "/movies",
-        element: <Movies />,
-      },
-      {
-        path: "/tv",
-        element: <TvShows />,
-      },
-      {
-        path: "/detail/:type/:movieId",
-        element: <Detail />,
-      },
-      {
-        path: "/search/:keyword",
-        element: <SearchResults />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Trending />,
+        },
+        {
+          path: "/trending",
+          element: <Trending />,
+        },
+        {
+          path: "/movies",
+          element: <Movies />,
+        },
+        {
+          path: "/tv",
+          element: <TvShows />,
+        },
+        {
+          path: "/detail/:type/:movieId",
+          element: <Detail />,
+        },
+        {
+          path: "/search/:keyword",
+          element: <SearchResults />,
+        },
+      ],
+    },
+  ],
+  { basename: "/tmdb-frontend" },
+);
 
 const App = () => {
   return <RouterProvider router={router} />;
