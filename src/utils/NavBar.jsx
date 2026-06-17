fcc28f535b3d757d0df02f8f0a3fc697d0e3e97c
Ameya Kale <ameya.k.kale@gmail.com>
2026-06-17 01:02:08 -0500

    changing logo

--- src/utils/NavBar.jsx
+++ src/utils/NavBar.jsx
@@ -37,9 +37,12 @@ const NavBar = () => {
 
     return (
         <div className="w-screen p-5 flex gap-4 items-center">
-            <Link to='/'>
+            <Link 
+                to='/'
+                onClick={() => updateActivePage(filters[0].value)}
+            >
                 <img
-                    src="https://files.readme.io/29c6fee-blue_short.svg"
+                    src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"
                     alt="TMDB Logo"
                     className="h-7 w-20"
                 ></img>
